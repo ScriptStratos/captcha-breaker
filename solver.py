b5b755417de5111057db31aed3dc1268ba1ca22e
Alex Stratos <alex@scriptstratos.dev>
2025-11-14 18:20:44 -0500

    fix race condition in preprocessor

--- solver.py
+++ solver.py
@@ -7,6 +7,7 @@ from loguru import logger
 
 from model import create_model
 # Refactored solver - 2026-03-11
+# Refactored solver - 2026-03-11
 # Refactored model - 2026-03-11
 # Refactored preprocessor - 2026-03-11
 # Refactored decoder - 2026-03-11
